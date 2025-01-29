d848306351446ae24adfd525446cee4578276ce6
Ujjwal789 <156940582+Ujjwal789@users.noreply.github.com>
2025-01-29 14:42:11 +0530

    Btu project

---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MessageSimple, Channel } from 'stream-chat-react';
+import { ChannelInner, CreateChannel, EditChannel } from './';
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    const renderMessage = (messageProps, i) => <MessageSimple key={i} {...messageProps} />;
+
+    if (isCreating) {
+        return (
+            <div className="channel__container">
+                <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+            </div>
+        );
+    }
+
+    if (isEditing) {
+        return (
+            <div className="channel__container">
+                <EditChannel setIsEditing={setIsEditing} />
+            </div>
+        );
+    }
+
+    const EmptyState = () => (
+        <div className="channel-empty__container">
+            <p className="channel-empty__first">This is the beginning of your chat history.</p>
+            <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
+        </div>
+    );
+
+    return (
+        <div className="channel__container">
+            <Channel EmptyStateIndicator={EmptyState} Message={renderMessage}>
+                <ChannelInner setIsEditing={setIsEditing} />
+            </Channel>
+        </div>
+    );
+}
+
+export default ChannelContainer;
